25540c2cbdf15026bc5ad114f79bfce1bf892fbd
Your Name <you@example.com>
2026-06-20 14:27:59 +0000

    ...

--- ghidra-rpc-server/build-containers.sh
+++ ghidra-rpc-server/build-containers.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
-docker build . --target ghidra-server -t ghcr.io/trolldemorted/ghidra-server
-docker build . --target ghidra-rpc --build-context cli=../ghidra-headless-cli -t ghcr.io/trolldemorted/ghidra-rpc
+docker build . --target ghidra-server -t ghcr.io/trolldemorted/ghidra-headless-cli/ghidra-server
+docker build . --target ghidra-rpc --build-context cli=../ghidra-headless-cli -t ghcr.io/trolldemorted/ghidra-headless-cli/ghidra-rpc
